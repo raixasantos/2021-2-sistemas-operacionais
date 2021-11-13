712791266d2b7ca63be03e9322ab071b5c0b9ea8
Raíssa Santos <raixapsantos@gmail.com>
2021-11-13 19:12:57 -0300

    Adicionando: multiplicação de P elementos

--- trabalho-01-multiplicacao-de-matrizes/threads.cpp
+++ trabalho-01-multiplicacao-de-matrizes/threads.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <thread>
 #include <vector>
-#include <string>
+#include <cmath>
 #include <time.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <thread>
 
 using namespace std;
 
@@ -52,26 +50,49 @@ vector<vector<int>> ler_matriz(char *arquivo){
 
 void multiplicar_matrizes(vector<vector<int>> & m1, 
                             vector<vector<int>> & m2, clock_t inicio,
-                            int indice){  
+                            int indice, int lc, int cc,
+                            int lf, int cf){   
 
+    int n_1 = m1[0][0], m_2 = m2[0][1];
+    int aux_c = 0, aux_l_t = 0, aux_l = 0, cc_maior = 0;
+    if(lf - lc > 0){ // verificação de linhas para pular
+        cc_maior = 1;
+        aux_c = cf;
+        aux_l_t = lf - lc;
+        cf = m_2;
+    }  
+ 
+    vector<int> resultado;
     int somaprod = 0;
-    for(int i = 1; i < m1[0][0]+1; i++){
-        for(int j = 0; j < m2[0][1]; j++){
+    for(int linha = lc; linha <= lf; linha++){
+        for(int coluna = cc; coluna < cf; coluna++){
             somaprod = 0;
-            for(int a = 1; a < m1[0][1]+1; a++){
-                somaprod += m1[i][a-1] * m2[a][j];
+            for(int a = 1; a < n_1+1; a++){
+                somaprod += m1[linha][a-1] * m2[a][coluna];
             }
+            resultado.push_back(somaprod);
         }
-    }    
-
+        if(cc_maior){
+            cc = 0;
+            if(aux_l == aux_l_t-1){
+                cf = aux_c;
+            }else{
+                cf = m_2;
+            }
+            aux_l++;
+        }
+    }   
 
     clock_t fim;
     fim = clock();
+
     string nome_arquivo = "matriz_threads" + to_string(indice) + ".txt";
     ofstream out(nome_arquivo);
     out << m1[0][0] << " " <<  m2[0][1] << endl;  
-    // inserir os elementos
-    out << "tempo: "  << (double)(fim - inicio) / CLOCKS_PER_SEC << endl;
+    for(int i = 0; i < resultado.size(); i++)
+        out << resultado[i] << endl;
+    out << "tempo: "  << (double)(fim - inicio) / CLOCKS_PER_SEC;
+    resultado.clear();
 }
 
 int main(int argc, char *argv[]){
@@ -84,18 +105,37 @@ int main(int argc, char *argv[]){
     ss >> P;
     int n1 = matriz1[0][0], // linhas
         m2 = matriz2[0][1]; // colunas
-    int qnt_arquivos = ceil((n1*m2)/P);
+    int qnt_arquivos = ceil((double) (n1*m2)/P); 
 
     vector<thread> threads; // o vetor de threads
 
-    for(int i = 0; i < qnt_arquivos;i++){        
+    int linha_c = 0, coluna_c = 0, linha_f = 0, coluna_f = 0;  
+    for(int i = 0; i < qnt_arquivos; i++){    
+        if(i > 0){
+            linha_c = (P*i)/n1;
+            coluna_c = (P*i)%n1;           
+        }
+        else{
+            linha_c = 0;
+            coluna_c = 0;
+        }
+
+        if(i == qnt_arquivos-1){
+            linha_f = n1-1;
+            coluna_f = m2-1;
+        }
+        else{
+            linha_f = ((P*(i+1))-1)/n1;
+            coluna_f = ((P*(i+1))-1)%n1;
+        }
+
         clock_t inicio;
         inicio = clock();
-        // criar thread 
-        threads.push_back(thread(multiplicar_matrizes, ref(matriz1), ref(matriz2), inicio, i));
+        threads.push_back(thread(multiplicar_matrizes, ref(matriz1), ref(matriz2), inicio, i, 
+                                linha_c+1, coluna_c, linha_f+1, coluna_f+1)); 
     }
 
-    for(auto &thread : threads){ // esperar todas as threads terminarem
+    for(auto &thread : threads){ 
         thread.join();
     }
 
